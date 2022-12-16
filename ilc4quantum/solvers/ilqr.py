c16bc95b41a014857c1395cd486f318b2c69b80e
andy <agoldschmidt11@gmail.com>
2022-12-16 14:35:24 -0600

    Fix slew

--- ilc4quantum/solvers/ilqr.py
+++ ilc4quantum/solvers/ilqr.py
@@ -23,7 +23,7 @@ def ilqr(
     Iterative LQR with box constraints (1st order in dynamics, 2nd order in cost).
 
     Notes:  1.  It is recommended to use 'jax_enable_x64' for iLQR.
-            2.  TODO: Slew bounds not working?
+            2.  Slew bounds must account for the rate from the control guess.
 
     :param x_init: Initial state
     :param tx_guess: Shape is (time, state)
@@ -69,6 +69,11 @@ def iteration_lqr(
     _, n_state = tx_guess.shape
     tz_guess = jnp.concatenate([tx_guess, jnp.vstack([tu_guess, jnp.zeros(n_ctrl)])], axis=1)
 
+    # Accumulated slew
+    # TODO: Initial slew?
+    slew_init = jnp.zeros(n_ctrl)
+    tu_guess_slew = jnp.vstack([slew_init[None], tu_guess[1:] - tu_guess[:-1]])
+
     # 1. Compute derivatives
     # -- Linear expansion of model
     tF_linear = jax.vmap(linear_model_fn)(tz_guess[:-1])
@@ -80,10 +85,11 @@ def iteration_lqr(
     fwd = jnp.zeros(n_ctrl)
     V_x = tj_cost[-1, :n_state]
     V_xx = tH_cost[-1, :n_state, :n_state]
-    # -- Run scan backward TODO: search mu?
+    # -- Run scan backward
+    # TODO: Search mu?
     _, feeds = jax.lax.scan(jax.tree_util.Partial(scan_backward, mu=1e-3, u_sat=u_sat, du_sat=du_sat, prob=prob),
                             (fwd, V_x, V_xx),
-                            (tu_guess, tj_cost, tH_cost, tF_linear),
+                            (tu_guess, tu_guess_slew, tj_cost, tH_cost, tF_linear),
                             reverse=True)
     feedforwards, Feedbacks, delta_V1s, delta_V2s = feeds
 
@@ -96,30 +102,38 @@ def iteration_lqr(
     # wolfe_c2 = 0.9
     # dcost_quad = jnp.sum(delta_V2s, axis=0)
 
-    local_cond_fn = jax.tree_util.Partial(cond_fn,
-                                          x_init=x_init,
-                                          scan=(tz_guess[:-1], feedforwards, Feedbacks),
-                                          u_sat=u_sat,
-                                          model_fn=model_fn,
-                                          cost_fn=cost_fn,
-                                          cost_guess=cost_guess,
-                                          dcost_lin=dcost_lin,
-                                          wolfe_c1=wolfe_c1,
-                                          min_step=min_step)
+    local_cond_fn = jax.tree_util.Partial(
+        cond_fn,
+        x_init=x_init,
+        scan=(tz_guess[:-1], tu_guess_slew, feedforwards, Feedbacks),
+        u_sat=u_sat,
+        du_sat=du_sat,
+        model_fn=model_fn,
+        cost_fn=cost_fn,
+        cost_guess=cost_guess,
+        dcost_lin=dcost_lin,
+        wolfe_c1=wolfe_c1,
+        min_step=min_step
+    )
 
+    # TODO: How to improve the n_eval here?
     step = jax.lax.while_loop(local_cond_fn, body_fn, 1.)
     step = jnp.where(step <= min_step, 0., step)
-    x_end, zs_opt = jax.lax.scan(jax.tree_util.Partial(scan_forward, step=step, u_sat=u_sat, model_fn=model_fn),
-                                 x_init,
-                                 (tz_guess[:-1], feedforwards, Feedbacks))
+    x_end, zs_opt = jax.lax.scan(
+        jax.tree_util.Partial(scan_forward, step=step, u_sat=u_sat, du_sat=du_sat, model_fn=model_fn),
+        x_init,
+        (tz_guess[:-1], tu_guess_slew, feedforwards, Feedbacks)
+    )
     xs_guess_next = jnp.vstack([zs_opt[:, :n_state], x_end])
     us_guess_next = zs_opt[:, -n_ctrl:]
     return (xs_guess_next, us_guess_next), step
 
 
-def cond_fn(s, x_init, scan, u_sat, model_fn, cost_fn, cost_guess, dcost_lin, wolfe_c1, min_step):
+def cond_fn(s, x_init, scan, u_sat, du_sat, model_fn, cost_fn, cost_guess, dcost_lin, wolfe_c1, min_step):
     # scan = (zs_guess[:-1], feedforwards, Feedbacks)
-    _, zs = jax.lax.scan(jax.tree_util.Partial(scan_forward, step=s, u_sat=u_sat, model_fn=model_fn), x_init, scan)
+    _, zs = jax.lax.scan(
+        jax.tree_util.Partial(scan_forward, step=s, u_sat=u_sat, du_sat=du_sat, model_fn=model_fn), x_init, scan
+    )
     armijo_cond = (s > min_step) & (cost_fn(zs) > cost_guess + wolfe_c1 * s * dcost_lin)
     return armijo_cond
 
@@ -129,24 +143,30 @@ def body_fn(s, discount=0.5):
     return s * discount
 
 
-def scan_forward(x, scan, step, u_sat, model_fn):
-    z_guess, feedforward, Feedback = scan
+def scan_forward(x, scan, step, u_sat, du_sat, model_fn):
+    z_guess, u_guess_slew, feedforward, Feedback = scan
     n_ctrl, n_state = Feedback.shape
-    u = jax.lax.clamp(-u_sat,
-                      z_guess[-n_ctrl:] + step * feedforward + Feedback @ (x - z_guess[:n_state]),
-                      u_sat)
-    z = jnp.hstack((x, u))
+
+    # Clip
+    # TODO: Is |fwd| <= du_sat? Would Feedback ever exceed clip?
+    du = jnp.clip(
+        step * feedforward + Feedback @ (x - z_guess[:n_state]),
+        a_min=-jnp.minimum(du_sat + u_guess_slew, u_sat + z_guess[-n_ctrl:]),
+        a_max=jnp.minimum(du_sat - u_guess_slew, u_sat - z_guess[-n_ctrl:])
+    )
+    z = jnp.hstack((x, z_guess[-n_ctrl:] + du))
     return model_fn(z), z
 
 
 def scan_backward(carry, scan, mu, u_sat, du_sat, prob):
     next_fwd, next_V_x, next_V_xx = carry
-    u_guess, J_cost, H_cost, F_linear = scan
+    u_guess, u_guess_slew, J_cost, H_cost, F_linear = scan
     # Pay attention to shapes!
     n_ctrl = len(u_guess)
     n_state = len(next_V_x)
 
     # 1. Approximate the Bellman value function
+    # TODO: Make this DDP for faster convergence
     F_x = F_linear[:, :n_state]
     F_u = F_linear[:, -n_ctrl:]
     Q_u = J_cost[-n_ctrl:] + F_u.T @ next_V_x
@@ -158,14 +178,14 @@ def scan_backward(carry, scan, mu, u_sat, du_sat, prob):
 
     # 2. Optimize
     # -- Control inequality constraints.
-    du_min = -jnp.minimum(du_sat, u_sat + u_guess)
-    du_max = jnp.minimum(du_sat, u_sat - u_guess)
+    du_min = -jnp.minimum(du_sat + u_guess_slew, u_sat + u_guess)
+    du_max = jnp.minimum(du_sat - u_guess_slew, u_sat - u_guess)
     # -- Solve the box QP (warm start with prev. step) (fwd = -Q_uu_inv @ Q_u)
     fwd, state = prob.run(next_fwd, params_obj=(Q_uu, Q_u), params_ineq=(du_min, du_max))
 
     # 3. Compute Feedback
     # -- Feedback for free controls. Zero for clamped.
-    clamped = jnp.repeat(((fwd <= du_min) & (fwd >= du_max))[:, None], n_state, axis=1)
+    clamped = jnp.repeat(((fwd <= du_min) | (fwd >= du_max))[:, None], n_state, axis=1)
     Bwd = jnp.where(clamped, jnp.zeros((n_ctrl, n_state)), -jnp.linalg.pinv(Q_uu) @ Q_ux)
 
     # 4. Backward update for the value
--- ilc4quantum/solvers/sqp.py
+++ ilc4quantum/solvers/sqp.py
@@ -48,6 +48,7 @@ def sqp(
                                                'approx_cost': quadraticize_cost,
                                                'u_sat': u_sat,
                                                'du_sat': du_sat})
+
     # Scan
     (tx, tu), steps = jax.lax.scan(local_iteration, (tx_guess, tu_guess), None, length=max_iter)
     return jnp.concatenate([tx, jnp.vstack([tu, jnp.zeros(tu.shape[1])])], axis=1)
@@ -159,7 +160,8 @@ def quad_program(
 
         Notes for OSQP:
          * Careful with the signature of linalg.block_diag versus sparse.block_diag.
-         * The arguments P and A are callable matvec objects (TODO: can these be made JAX sparse?)
+         * The arguments P and A can be parameters to callable matvec functions (allowing sparse). BCOO matrices must
+           specify a concrete number for nse (number of specified elements).
     """
     # Pay attention to shapes!
     n_horiz, n_ctrl = tu_g.shape
@@ -167,7 +169,7 @@ def quad_program(
 
     # - quadratic objective: (1/2)zHz
     tH_final = jnp.zeros_like(tH_cost[-1, :n_state, :n_state])
-    P_qp = block_diag(*tH_cost, tH_final) / 2  # sparse.BCOO.fromdense?
+    P_qp = block_diag(*tH_cost, tH_final) / 2
     nse_P_qp = n_horiz * (n_state + n_ctrl) ** 2 + n_state ** 2
 
     # - linear objective: Jz
@@ -175,6 +177,7 @@ def quad_program(
     q_qp = jnp.hstack([tj_cost.flatten(), tJ_final])
 
     # - initial condition: dxs[0] == x_init - xs_g[0]
+    # TODO: Allow for initial control specification?
     I_eq = jnp.hstack([jnp.eye(n_state), jnp.zeros((n_state, n_horiz * (n_state + n_ctrl)))])
     nse_I_eq = n_state
     lo_eq_init = x_init - tx_g[0]
@@ -183,8 +186,10 @@ def quad_program(
     # - linear dynamics: dxs[t+1] == F dzs[t] + rs[t]
     # dxs[t+1] = S dzs[t]
     id_x = jnp.hstack([jnp.eye(n_state), jnp.zeros((n_state, n_ctrl))])
-    S_eq = jnp.hstack([jnp.zeros((n_horiz * n_state, n_state + n_ctrl)),
-                       block_diag(jnp.kron(jnp.eye(n_horiz - 1), id_x), jnp.eye(n_state))])
+    S_eq = jnp.hstack([
+        jnp.zeros((n_horiz * n_state, n_state + n_ctrl)),
+        block_diag(jnp.kron(jnp.eye(n_horiz - 1), id_x), jnp.eye(n_state))
+    ])
     # F dzs[t]
     F_eq = jnp.hstack([block_diag(*tF_lin), jnp.zeros((n_state * n_horiz, n_state))])
     # (S - F) dzs[t] := dxs[t+1] - F dzs[t]
@@ -194,39 +199,143 @@ def quad_program(
     up_eq = lo_eq
 
     # - control inequality constraints
-    broadcast = jnp.ones((n_horiz, n_ctrl))
     # -u < u_sat --> -u_g -(u - u_g) < u_sat
     # u < u_sat --> u_g + (u - u_g) < u_sat
-    tu_min = -jnp.minimum(du_sat * broadcast, u_sat * broadcast + tu_g)
-    tu_max = jnp.minimum(du_sat * broadcast, u_sat * broadcast - tu_g)
+    tu_du_min = -(u_sat * jnp.ones((n_horiz, n_ctrl)) + tu_g)
+    tu_du_max = u_sat * jnp.ones((n_horiz, n_ctrl)) - tu_g
 
     # - state inequality constraints
     x_min = -jnp.inf
     x_max = jnp.inf
 
-    # - combined inequality constraints
-    Aineq = jnp.hstack([jnp.eye(n_horiz * (n_state + n_ctrl)),
-                        jnp.zeros((n_horiz * (n_state + n_ctrl), n_state))])
-    lo_ineq = jnp.concatenate((x_min * jnp.ones((n_horiz, n_state)), tu_min), axis=1).flatten()
-    up_ineq = jnp.concatenate((x_max * jnp.ones((n_horiz, n_state)), tu_max), axis=1).flatten()
-    nse_A_ineq = n_horiz * (n_state + n_ctrl)
+    # - combined saturation inequality constraints
+    A_ineq_sat = jnp.hstack([
+        jnp.eye(n_horiz * (n_state + n_ctrl)), jnp.zeros((n_horiz * (n_state + n_ctrl), n_state))
+    ])
+    # Pair up each (dx_sat(t), du_sat(t)), then flatten.
+    lo_ineq_sat = jnp.concatenate((x_min * jnp.ones((n_horiz, n_state)), tu_du_min), axis=1).flatten()
+    up_ineq_sat = jnp.concatenate((x_max * jnp.ones((n_horiz, n_state)), tu_du_max), axis=1).flatten()
+    nse_A_ineq_sat = n_horiz * (n_state + n_ctrl)
+
+    # - control slew inequality constraints
+    slew_block_minus = jnp.kron(
+        jnp.eye(n_horiz), jnp.hstack([jnp.zeros((n_ctrl, n_state)), -jnp.eye(n_ctrl)])
+    )[:-n_ctrl]
+    slew_block_plus = jnp.kron(
+        jnp.eye(n_horiz), jnp.hstack([jnp.zeros((n_ctrl, n_state)), jnp.eye(n_ctrl)])
+    )[n_ctrl:]
+    A_ineq_slew = jnp.concatenate([slew_block_minus + slew_block_plus,
+                                   jnp.zeros((n_ctrl * (n_horiz - 1), n_state))], axis=1)
+    tu_g_slew = tu_g[1:] - tu_g[:-1]
+    lo_ineq_slew = -(du_sat * jnp.ones((n_horiz - 1, n_ctrl)) + tu_g_slew).flatten()
+    up_ineq_slew = (du_sat * jnp.ones((n_horiz - 1, n_ctrl)) - tu_g_slew).flatten()
+    nse_A_ineq_slew = 2 * n_ctrl * (n_horiz - 1)
 
     # - OSQP constraints
-    A_qp = jnp.vstack([I_eq, A_eq, Aineq])  # sparse.BCOO.fromdense?
-    lo_qp = jnp.hstack([lo_eq_init, lo_eq, lo_ineq])
-    up_qp = jnp.hstack([up_eq_init, up_eq, up_ineq])
+    A_qp = jnp.vstack([I_eq, A_eq, A_ineq_sat, A_ineq_slew])
+    lo_qp = jnp.hstack([lo_eq_init, lo_eq, lo_ineq_sat, lo_ineq_slew])
+    up_qp = jnp.hstack([up_eq_init, up_eq, up_ineq_sat, up_ineq_slew])
 
     # - OSQP Solve
     # If required the algorithm can be sped up by setting check_primal_dual_infeasability to False,
     # and by setting eq_qp_preconditioner to "jacobi" (when possible).
     prob = BoxOSQP(matvec_Q=sparse_matvec, matvec_A=sparse_matvec)
     sp_P_qp = sparse.BCOO.fromdense(P_qp, nse=nse_P_qp)
-    sp_A_qp = sparse.BCOO.fromdense(A_qp, nse=nse_I_eq + nse_A_eq + nse_A_ineq)
+    sp_A_qp = sparse.BCOO.fromdense(A_qp, nse=nse_I_eq + nse_A_eq + nse_A_ineq_sat + nse_A_ineq_slew)
     params, state = prob.run(params_obj=(sp_P_qp, q_qp), params_eq=sp_A_qp, params_ineq=(lo_qp, up_qp))
 
     # params.primal[0] = [dx(0),du(0),dx(1),du(1),...,dx(H-1),du(H-1),dx(H)
     res = jnp.hstack([params.primal[0], jnp.zeros(n_ctrl)]).reshape(n_horiz + 1, n_state + n_ctrl)
     return res[:, :n_state], res[:-1, -n_ctrl:]
+
+
+# def quad_program(
+#         x_init,
+#         tx_g,
+#         tu_g,
+#         tF_lin,
+#         tr_feas,
+#         tH_cost,
+#         tj_cost,
+#         u_sat,
+#         du_sat):
+#     """
+#         Solve an MPC iteration with a quadratic program.
+#
+#         Cast MPC problem to a QP: x = [dx(0),du(0),dx(1),du(1),...,dx(H-1),du(H-1),dx(H)] where the parameter H is the
+#          horizon (H >= 1). All xs variables have length H + 1. Meanwhile, Hs_cost, Js_cost, and Fs_cost have length H.
+#          This means we must append (or pass an argument for) the appropriate x(H) action--we choose to append here.
+#
+#         Notes for OSQP:
+#          * Careful with the signature of linalg.block_diag versus sparse.block_diag.
+#          * The arguments P and A can be parameters to callable matvec functions (allowing sparse).
+#     """
+#     # Pay attention to shapes!
+#     n_horiz, n_ctrl = tu_g.shape
+#     _, n_state = tx_g.shape
+#
+#     # - quadratic objective: (1/2)zHz
+#     tH_final = jnp.zeros_like(tH_cost[-1, :n_state, :n_state])
+#     P_qp = block_diag(*tH_cost, tH_final) / 2  # sparse.BCOO.fromdense?
+#     nse_P_qp = n_horiz * (n_state + n_ctrl) ** 2 + n_state ** 2
+#
+#     # - linear objective: Jz
+#     tJ_final = jnp.zeros_like(tj_cost[-1, :n_state])
+#     q_qp = jnp.hstack([tj_cost.flatten(), tJ_final])
+#
+#     # - initial condition: dxs[0] == x_init - xs_g[0]
+#     I_eq = jnp.hstack([jnp.eye(n_state), jnp.zeros((n_state, n_horiz * (n_state + n_ctrl)))])
+#     nse_I_eq = n_state
+#     lo_eq_init = x_init - tx_g[0]
+#     up_eq_init = lo_eq_init
+#
+#     # - linear dynamics: dxs[t+1] == F dzs[t] + rs[t]
+#     # dxs[t+1] = S dzs[t]
+#     id_x = jnp.hstack([jnp.eye(n_state), jnp.zeros((n_state, n_ctrl))])
+#     S_eq = jnp.hstack([jnp.zeros((n_horiz * n_state, n_state + n_ctrl)),
+#                        block_diag(jnp.kron(jnp.eye(n_horiz - 1), id_x), jnp.eye(n_state))])
+#     # F dzs[t]
+#     F_eq = jnp.hstack([block_diag(*tF_lin), jnp.zeros((n_state * n_horiz, n_state))])
+#     # (S - F) dzs[t] := dxs[t+1] - F dzs[t]
+#     A_eq = S_eq - F_eq
+#     nse_A_eq = n_horiz * (n_state + n_ctrl) ** 2 + n_state ** 2
+#     lo_eq = tr_feas.flatten()
+#     up_eq = lo_eq
+#
+#     # - control inequality constraints
+#     broadcast = jnp.ones((n_horiz, n_ctrl))
+#     # -u < u_sat --> -u_g -(u - u_g) < u_sat
+#     # u < u_sat --> u_g + (u - u_g) < u_sat
+#     tu_min = -jnp.minimum(du_sat * broadcast, u_sat * broadcast + tu_g)
+#     tu_max = jnp.minimum(du_sat * broadcast, u_sat * broadcast - tu_g)
+#
+#     # - state inequality constraints
+#     x_min = -jnp.inf
+#     x_max = jnp.inf
+#
+#     # - combined inequality constraints
+#     Aineq = jnp.hstack([jnp.eye(n_horiz * (n_state + n_ctrl)),
+#                         jnp.zeros((n_horiz * (n_state + n_ctrl), n_state))])
+#     lo_ineq = jnp.concatenate((x_min * jnp.ones((n_horiz, n_state)), tu_min), axis=1).flatten()
+#     up_ineq = jnp.concatenate((x_max * jnp.ones((n_horiz, n_state)), tu_max), axis=1).flatten()
+#     nse_A_ineq = n_horiz * (n_state + n_ctrl)
+#
+#     # - OSQP constraints
+#     A_qp = jnp.vstack([I_eq, A_eq, Aineq])  # sparse.BCOO.fromdense?
+#     lo_qp = jnp.hstack([lo_eq_init, lo_eq, lo_ineq])
+#     up_qp = jnp.hstack([up_eq_init, up_eq, up_ineq])
+#
+#     # - OSQP Solve
+#     # If required the algorithm can be sped up by setting check_primal_dual_infeasability to False,
+#     # and by setting eq_qp_preconditioner to "jacobi" (when possible).
+#     prob = BoxOSQP(matvec_Q=sparse_matvec, matvec_A=sparse_matvec)
+#     sp_P_qp = sparse.BCOO.fromdense(P_qp, nse=nse_P_qp)
+#     sp_A_qp = sparse.BCOO.fromdense(A_qp, nse=nse_I_eq + nse_A_eq + nse_A_ineq)
+#     params, state = prob.run(params_obj=(sp_P_qp, q_qp), params_eq=sp_A_qp, params_ineq=(lo_qp, up_qp))
+#
+#     # params.primal[0] = [dx(0),du(0),dx(1),du(1),...,dx(H-1),du(H-1),dx(H)
+#     res = jnp.hstack([params.primal[0], jnp.zeros(n_ctrl)]).reshape(n_horiz + 1, n_state + n_ctrl)
+#     return res[:, :n_state], res[:-1, -n_ctrl:]
 
 
 @sparse.sparsify
